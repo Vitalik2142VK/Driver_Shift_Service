07b7787000c89ab5c620a8f11d034c1267a4f64e
Vitalii <lennab@mail.ru>
2025-06-24 17:39:00 +0300

    First commit

---
+++ driver-shift-bot/src/main/java/ru/driver_shift_service/bot/DriverShiftBotApplication.java
@@ -0,0 +1,12 @@
+package ru.driver_shift_service.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverShiftBotApplication
+{
+    public static void main(String[] args) {
+        SpringApplication.run(DriverShiftBotApplication.class, args);
+    }
+}
---
+++ driver-shift-bot/src/test/java/ru/driver_shift_service/bot/DriverShiftBotApplicationTest.java
@@ -0,0 +1,38 @@
+package ru.driver_shift_service.bot;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class DriverShiftBotApplicationTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public DriverShiftBotApplicationTest(String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( DriverShiftBotApplicationTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
