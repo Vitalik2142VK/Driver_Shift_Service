f2c2428553513fbc56eadb8a3d09cfe21647e353
Vitalik2142VK <120681864+Vitalik2142VK@users.noreply.github.com>
2026-03-11 16:42:07 +0300

    In pocess

---
+++ bot_data_handler/src/main/java/ru/driver_shift_service/bot_data_handler/ShiftDriversBotApplication.java
@@ -0,0 +1,11 @@
+package ru.driver_shift_service.bot_data_handler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftDriversBotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShiftDriversBotApplication.class, args);
+    }
+}
